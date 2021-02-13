fcc8f853b83952d40429bd36c8918596a13d5cfd
Thiago Valverde de Souza <valverde.thiago@gmail.com>
2021-02-13 17:24:30 -0300

    Customizing database configuration for tests

--- src/test/groovy/com/altruist/config/RepositoryConfiguration.groovy
+++ src/test/groovy/com/altruist/config/RepositoryConfiguration.groovy
@@ -1,8 +1,7 @@
 package com.altruist.config
 
-import com.opentable.db.postgres.embedded.EmbeddedPostgres
+
 import groovy.sql.Sql
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
@@ -13,21 +12,9 @@ import java.sql.SQLException
 @Configuration
 class RepositoryConfiguration {
 
-    @Value('${test-db.port}')
-    int port;
-
     @Profile("test")
     @Bean(destroyMethod = "close")
     Sql sql(DataSource dataSource) throws SQLException {
         return new Sql(dataSource)
-    }
-
-    @Profile("test")
-    @Bean
-    DataSource dataSource() {
-        EmbeddedPostgres embeddedPostgres = EmbeddedPostgres.builder()
-                .setPort(port)
-                .start();
-        return embeddedPostgres.getPostgresDatabase();
     }
 }
