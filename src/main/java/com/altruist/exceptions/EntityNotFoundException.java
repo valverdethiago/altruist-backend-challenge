0ee194668d709bb8ff7e7875be3a11cb17cd7b87
Thiago Valverde de Souza <valverde.thiago@gmail.com>
2021-02-14 20:41:20 -0300

    Minor refactor to delete unused constructor

--- src/main/java/com/altruist/exceptions/EntityNotFoundException.java
+++ src/main/java/com/altruist/exceptions/EntityNotFoundException.java
@@ -6,9 +6,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class EntityNotFoundException extends RuntimeException{
 
-    public EntityNotFoundException() {
-    }
-
     public EntityNotFoundException(String message) {
         super(message);
     }
